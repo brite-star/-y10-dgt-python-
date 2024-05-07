195f354e2f47def82fcadd85ee1b84e801482919
brite-star <168596823+brite-star@users.noreply.github.com>
2024-05-08 10:03:44 +1200

    Add files via upload

---
+++ #functions go here.py
@@ -0,0 +1,9 @@
+#functions go here 
+def intro(): 
+    print("Welcome")
+    print()
+    print("Instructions go here")
+    return "" # I am returing (displaying/using) all items in the code.
+
+# Main Routine 
+intro() #This calls the function so that we can use it. 
