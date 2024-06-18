68250905e0ad9edb6935d7c5aa8e4d3af3f54964
brite-star <168596823+brite-star@users.noreply.github.com>
2024-06-19 10:19:33 +1200

    Update Fence Cost Calculator.py

--- Fence Cost Calculator.py
+++ Fence Cost Calculator.py
@@ -32,38 +32,46 @@ def test_float_Num(question):
         print(question)
         try: 
             num = float(input())
+            if num > 0:
+                return num
+            else:
+                print(error)
 
         except ValueError:
             print(error)
-        return (num)
+        
     
  
-# Main routine 
-width = test_float_Num("Enter width of your area:")
-print(f"The number you have entered is ✨ {width} ✨.")
-print("")
-length = test_float_Num("Enter length of your area:")
-print(f"The number you have entered is ✨ {length} ✨.")
-print("")
-costpersquaremeter = test_float_Num("Enter the cost per meter squared:")
-print(f"The number you have entered is ✨ {costpersquaremeter} ✨.")
+# Main routine
+done = ""
+while done == "":
+    width = test_float_Num("Enter width of your area:")
+    print(f"The number you have entered is ✨ {width} ✨.")
+    print("")
+    length = test_float_Num("Enter length of your area:")
+    print(f"The number you have entered is ✨ {length} ✨.")
+    print("")
+    costpersquaremeter = test_float_Num("Enter the cost per meter squared:")
+    print(f"The number you have entered is ✨ {costpersquaremeter} ✨.")
+    
+    #Adding up the numbers
+    perimeter= (width + length)*2
+    totalcost = perimeter*costpersquaremeter
+    
+    #Results
+    print("")
+    print(f"Thanks,{Username}!")
+    print("Here are your results:")
+    print("")
+    print("✨ r e s u l t s ✨")
+    print("---------------------------------------")
+    print("The permeter of your area is: ")
+    print(f"✨ {perimeter} m ✨")
+    print("")
+    print(f"The total cost of your area is: ")
+    print(f"✨ ${totalcost} ✨")
+    print("----------------------------------------")
+    done = input("If you would like to do another calculation, please press <enter> or any other key to quit.")
 
-#Adding up the numbers
-permeter= (width + length)*2
-totalcost = peremeter*costpersquaremeter
+print("Thanks for using my fence calculator. Have a nice day!")
 
-#Results
-print("")
-print(f"Thanks,{Username}!")
-print("Here are your results:")
-print("")
-print("✨ r e s u l t s ✨")
-print("---------------------------------------")
-print("The permeter of your area is: ")
-print(f"✨ {permeter} m ✨")
-print("")
-print(f"The total cost of your area is: ")
-print(f"✨ ${totalcost} ✨")
-print("----------------------------------------")
-
-#
